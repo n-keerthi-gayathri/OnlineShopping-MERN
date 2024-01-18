f19fdbe2bcf8d94f5c22f0ff80903539b14f00b2
n-keerthi-gayathri <118381441+n-keerthi-gayathri@users.noreply.github.com>
2024-01-18 13:29:59 +0530

    Update index.js for handling file uploads

--- backend/index.js
+++ backend/index.js
@@ -30,12 +30,21 @@ app.get("/",(req,res)=>{
 
 // Image Storage Engine
 
+// const storage = multer.diskStorage({
+//     destination: './upload/images',
+//     filename:(req,file,cb)=>{
+//         return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+//     }
+// 
 const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+  destination: function (req, file, cb) {
+    // Specify a writable directory path
+    cb(null, '/tmp/uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
 const upload = multer({storage:storage})
 
